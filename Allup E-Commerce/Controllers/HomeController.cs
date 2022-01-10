d440a2e8728c1eda76a3d4f9e306ce7e74f7ad79
ayhanexe <aykhansab@code.edu.az>
2022-01-10 22:56:15 +0400

    initial commit

---
+++ Allup E-Commerce/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Allup_E_Commerce.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Allup_E_Commerce.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            string rootPath = _webHostEnvironment.WebRootPath;
+            ViewData["rootPath"] = rootPath;
+            return View();
+        }
+
+    }
+}
